fc991848f4952422029c2b703e583875435210b9
Danny <d.dee.humphreys@gmail.com>
2024-11-03 23:16:02 +0100

    Updated admin.py to register Voetbalspelers and Post models

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,7 @@
 # Register your models here.
+from .models import Voetbalspelers
 from django.contrib import admin
 from .models import Post
 
+admin.site.register(Voetbalspelers) 
 admin.site.register(Post)
